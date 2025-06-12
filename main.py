f5e305313f5c02afc52f40aa111aff7250a9f375
dalilamaia <dalilaegan@gmail.com>
2025-06-12 02:49:54 +0100

    repository done

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class TestUrbanRoutes:
     def test_order_2_ice_creams(self):
         # Adicionar em S8
         number_of_ice_creams = 2
-        for count in range(number_of_ice_creams):
+
         print("função criada para definir o pedido do sorvete")
         pass
 
