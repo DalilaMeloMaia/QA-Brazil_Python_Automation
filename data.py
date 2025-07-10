8b015a208f4579ecb516edd06e98d3537eaefb8c
dalilamaia <dalilaegan@gmail.com>
2025-07-10 01:17:07 +0100

    projeto 8

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-URBAN_ROUTES_URL = 'https://cnt-a6924e30-c9a3-4b63-8a3a-fae0cd7734a8.containerhub.tripleten-services.com?lng=pt'
+URBAN_ROUTES_URL = 'https://cnt-828e338d-98fc-4deb-ae15-b315f176c742.containerhub.tripleten-services.com/'
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
